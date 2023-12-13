2e2b571da383cd2e385cfb483f66c8ca7920d0b9
Andy Lang <djdreviz@gmail.com>
2023-12-14 00:38:57 +0200

    JPA Repository done

--- demo/src/main/java/com/andylang/demo/student/StudentService.java
+++ demo/src/main/java/com/andylang/demo/student/StudentService.java
@@ -2,18 +2,18 @@ package com.andylang.demo.student;
 
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(new Student(
-                1L,
-                "Andy Lang",
-                "andy.lang@email.com",
-                LocalDate.of(2000, Month.DECEMBER, 1),
-                43));
+        return studentRepository.findAll();
     }
 }
